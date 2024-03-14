aceccabc9d8904b88d70a681f45f6e4c4dbff6b9
pranav10231 <144907226+pranav10231@users.noreply.github.com>
2024-03-14 16:46:16 +0530

    Add files via upload

---
+++ AQuickDip/IfTest.java
@@ -0,0 +1,12 @@
+public class IfTest{
+	public static void main (String[] args){
+		int x=1;
+		if(x==3){
+		System.out.println("X must be 3");
+		}
+		System.out.println("This must run mo matter what...");
+	
+	}
+	
+
+}
